3e079c3a0bcaf2b938e56c5c1b881f1e8682d201
anishjogi172 <anishjogi7353012548@gmail.com>
2023-06-07 16:53:29 +0530

    added print statememt

--- src/main/java/com/prasad/springbootmongodb/model/Book_2.java
+++ src/main/java/com/prasad/springbootmongodb/model/Book_2.java
@@ -3,6 +3,8 @@ package com.prasad.springbootmongodb.model;
 public class Book_2 {
     public static void main(String[] args) {
         System.out.println("helo");
+        System.out.println("my name is Anish");
+        System.out.println("my name is not anish");
     }
 
 }
